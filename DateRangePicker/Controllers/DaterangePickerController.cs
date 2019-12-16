bacf28262151be75aef9dc882127956f0dcba3c1
Thao <thaonttth1807035@fpr.edu.vn>
2019-12-16 22:01:51 +0700

    start

---
+++ DateRangePicker/Controllers/DaterangePickerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateRangePicker.Controllers
+{
+    public class DaterangePickerController : Controller
+    {
+        // GET: DaterangePicker
+        public ActionResult Index(DateTime? start, DateTime? end)
+        {
+            ViewBag.start = start;
+            ViewBag.end = end;
+            return View();
+        }
+    }
+}
